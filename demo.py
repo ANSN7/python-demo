e046876edf0bbe8865835323a96378a7dced45ec
ANSN7 <89779680+ANSN7@users.noreply.github.com>
2022-12-07 11:42:19 +0000

    new version

--- demo.py
+++ demo.py
@@ -1,11 +1,10 @@
-import time
+from time import process_time
 
-st = time.process_time()
+st = process_time()
 sum_x = 0
 for i in range(1000000):
     sum_x += i
-time.sleep(3)
 print('Sum of first 1 million numbers is:', sum_x)
-et = time.process_time()
+et = process_time()
 res = et - st
 print('CPU Execution time:', res, 'seconds')
