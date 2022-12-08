4c7f326cd204e5a621b63029861fcd36e1a7a78e
ANSN7 <89779680+ANSN7@users.noreply.github.com>
2022-12-08 02:29:04 +0000

    added time

--- demo.py
+++ demo.py
@@ -1,13 +1,19 @@
 import timeit
 import math
+import time
+from datetime import datetime, timedelta
 
 start = timeit.default_timer()
+print(start)
+print(datetime.utcnow())
 # sum_x = 1
 # for i in range():
 #     sum_x += i
 sum_x = math.factorial(100000)
-print('Factorial is:', sum_x)
+# print('Factorial is:', sum_x)
 stop = timeit.default_timer()
+print(stop)
+print(datetime.utcnow())
 execution_time = stop - start
 
 print("CPU Execution time: "+str(execution_time)+'seconds')
