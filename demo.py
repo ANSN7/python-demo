345172388beeab4ff7f163d1597a0ca1f388ea33
ANSN7 <89779680+ANSN7@users.noreply.github.com>
2022-12-08 01:55:13 +0000

    changes

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ start = timeit.default_timer()
 # sum_x = 1
 # for i in range():
 #     sum_x += i
-sum_x = math.factorial(1000000)
+sum_x = math.factorial(100000)
 print('Factorial is:', sum_x)
 stop = timeit.default_timer()
 execution_time = stop - start
