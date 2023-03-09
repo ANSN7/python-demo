e28e3097df5106fdc7337c23d7e17e25981bfb1a
ANSN7 <89779680+ANSN7@users.noreply.github.com>
2023-03-09 21:31:34 +0000

    Update s3Ops.py

--- s3Ops.py
+++ s3Ops.py
@@ -5,4 +5,4 @@ s3 = boto3.client('s3', aws_access_key_id='ASIATZQTYOSCOTO3J6IH', aws_secret_acc
 bucket_name = 'webapp1buckett'
 s3_key = 'mem.sh'
 
-s3.upload_file(r"C:\Users\User\mem.sh", bucket_name, s3_key)
+s3.upload_file(r"\home\ec2-user\mem.sh", bucket_name, s3_key)
