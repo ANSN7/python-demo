53b01cdb219ac46494ecdea582f6bfa9f15187f4
ANSN7 <89779680+ANSN7@users.noreply.github.com>
2023-03-09 21:35:01 +0000

    Update s3Ops.py

--- s3Ops.py
+++ s3Ops.py
@@ -3,6 +3,6 @@ import boto3
 s3 = boto3.client('s3', aws_access_key_id='ASIATZQTYOSCOTO3J6IH', aws_secret_access_key='J5XpwUOLQ2ZMgi4JlSsxPwGzGfZHDC5G7fewznvE')
                   
 bucket_name = 'webapp1buckett'
-s3_key = 'mem.sh'
+s3_key = 'huppler2012.pdf'
 
-s3.upload_file(r"\home\ec2-user\mem.sh", bucket_name, s3_key)
+s3.upload_file("huppler2012.pdf", bucket_name, s3_key)
